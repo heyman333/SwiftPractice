f9d9c56e91a4c7d8ebf369a7a03029bf360140dc
HanYoungsoo <gglife333@gmail.com>
2017-04-22 19:03:13 +0900

    Protocol (1)

--- playground/Protocol.playground/Contents.swift
+++ playground/Protocol.playground/Contents.swift
@@ -15,3 +15,53 @@ struct RubyMember : SomePropertyProtocol {
     var description: String { return "name: \(self.name)" }
     
 }
+
+protocol MService {
+    
+    mutating func execute(cmd: String)
+    func showPort(p: Int) -> String
+}
+
+struct RubyMService : MService {
+    
+    var paramCommand : String?
+    
+    mutating func execute(cmd: String) {
+        self.paramCommand = cmd
+        if cmd == "start" { print("실행합니다.") }
+        
+    }
+    
+    func showPort(p:Int) -> String { return "Port : \(p), nowCommand: \(self.paramCommand!)" }
+    
+}
+
+var mService = RubyMService()
+mService.execute(cmd: "MyCommand!")
+print(mService.showPort(p: 8080))
+
+
+protocol SomeTypeProperty {
+    
+    static var defaultVal : String { get set }
+    static func getDefaultValue() -> String
+}
+
+struct TypeStruct : SomeTypeProperty {
+    
+    static var defaultVal: String = "default"
+    static func getDefaultValue() -> String {
+        return defaultVal
+    }
+}
+
+class ValurObject : SomeTypeProperty {
+    
+    static var defaultVal: String = "default"
+    class func getDefaultValue() -> String {
+        
+        return defaultVal
+    }
+    
+}
+
