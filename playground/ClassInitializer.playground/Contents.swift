eecf691eeb4e25123d7adc89e3f09d0e5da5ff34
HanYoungsoo <gglife333@gmail.com>
2017-05-10 10:38:26 +0900

    Class Initializer(3)

---
+++ playground/ClassInitializer.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+class MyClass {
+    
+    init() {
+        
+    }
+    // 지정 이니셜라이저
+    
+    convenience init() {
+        
+    }
+    // 편의 이니셜라이저
+    
+}
